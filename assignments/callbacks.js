5832207d8086b223827929588eb738d110642e52
eccoplex <eccothanks@gmail.com>
2019-09-17 17:55:49 -0500

    three callbacks complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,19 +41,46 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+
+getLength(items, (lengthOfList) => {
+  console.log(lengthOfList);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length-1]);
 }
+
+last(items, (lastItem) =>{
+  console.log(lastItem);
+});
+
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x, y);
 }
+
+const add(x, y) => {
+  return x + y;
+}
+console.log(sumNums(100, 50, add));
+
+
+
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x, y);
 }
+
+
+
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
